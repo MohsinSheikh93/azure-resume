a09897711aa0baa2d51ff4df41a5744275db31cb
MohsinSheikh93 <mohsinshaikh9277@gmail.com>
2024-05-04 11:23:20 +0500

    Created Azure Function API

--- frontend/main.js
+++ frontend/main.js
@@ -2,7 +2,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
     getVisitCount();
 })
 
-const funcApi = '';
+const funcApi = 'http://localhost:7071/api/GetResumeCounter';
 
 const getVisitCount = () => {
     let count = 0;
